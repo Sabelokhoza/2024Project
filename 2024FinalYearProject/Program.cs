e7a82b475cb4793beaab96688b20d7bcc9b08360
Sabelokhoza <161332464+Sabelokhoza@users.noreply.github.com>
2024-08-18 17:09:43 +0200

    added Models

--- 2024FinalYearProject/Program.cs
+++ 2024FinalYearProject/Program.cs
@@ -1,27 +1,52 @@
+using _2024FinalYearProject.Data;
+using _2024FinalYearProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+var connString = builder.Configuration.GetConnectionString("Connection");
+builder.Services.AddDbContext<AppDbContext>(opts =>
+opts.UseSqlServer(connString, opts =>
+{
+    opts.EnableRetryOnFailure();
+    opts.CommandTimeout(120);
+    opts.UseCompatibilityLevel(110);
+}));
+
+
+builder.Services.AddIdentityCore<AppUser>().AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<AppDbContext>();
+//builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+
+//Configure middleware
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
+//Configure middleware
 app.UseStaticFiles();
-
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+// route for paging all products
+app.MapControllerRoute(
+    name: "allpaging",
+    pattern: "{controller}/{action}/{id=all}/page{BookingPage}");
+
+// route for sorting
+app.MapControllerRoute(
+    name: "sortingcategory",
+    pattern: "{controller}/{action}/{id}/orderby{sortBy}");
+
+// least specific route - 0 required segments 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//SeedIdentityData.EnsureIdentityPopulated(app);
 
 app.Run();
